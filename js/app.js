ed0b17851f4c3f3177fc493dc89d6fe6eff076ea
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-01-30 15:50:06 +0100

    FEAT semicolons

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 // 1. chiedere nome all'utente
 
-let firstname = prompt('Quale è il tuo nome?')
-console.log(firstname)
+let firstname = prompt('Quale è il tuo nome?');
+console.log(firstname);
 
 // 2. chidere cognome all'utente
 
-let surname = prompt('Quale è il tuo cognome?')
-console.log(surname)
+let surname = prompt('Quale è il tuo cognome?');
+console.log(surname);
 
 // 3. chiedere colore preferito
 
-let  color = prompt('E il tuo colore preferito?')
-console.log(color)
+let  color = prompt('E il tuo colore preferito?');
+console.log(color);
 
 // 4. chiedere un numero
 
-let number = prompt('Dimmi un numero da 1 a 100')
-console.log(number)
+let number = prompt('Dimmi un numero da 1 a 100');
+console.log(number);
 
 // 5. Creare un messaggio
 
-let message = ('Qusta è la tua sicurissima password')
+let message = ('Qusta è la tua sicurissima password');
 
 // Creare una password con concatenamenti tramite js
 
-const password = document.getElementById('passwordCreated')
+const password = document.getElementById('passwordCreated');
 
 password.innerHTML = message + ' ' + '=' + ' ' + firstname + surname + color + number
-console.log(password)
+console.log(password);
