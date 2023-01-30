13ee2914008552c2a0729dd1c8f4db5747fe97ce
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-01-30 15:14:15 +0100

    EDIT js file + prompt

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// 1. chiedere nome all'utente
+let firstname = prompt('Quale è il tuo nome?')
+console.log(firstname)
+// 2. chidere cognome all'utente
+let surname = prompt('Quale è il tuo cognome?')
+console.log(surname)
+// 3. chiedere colore preferito
+let  color = prompt('E il tuo colore preferito?')
+console.log(color)
+// 4. chiedere un numero
+let number = prompt('Dimmi un numero da 1 a 100')
+console.log(number)
+// Creare una password con concatenamenti tramite js
+const  = document.getElementById('first-title')
